923fa6a177268aad47f33eed222f24922704651b
selfman2 <thonib@hotmail.com>
2015-03-13 15:05:06 +0100

    tested some code for the model + forecast; updated the presentation

--- code/test.R
+++ code/test.R
@@ -1,2 +1,28 @@
-# try
+# import data.frame
+sessionInfo()
+Sys.setlocale(category="LC_ALL", locale="de_DE")
+Sys.setlocale(category="LC_NUMERIC", locale="de_DE")
+### kind of workaround for messed up , and . delimiter of numbers
+### . in germany is used as 1000-delimiter, whereas , is used as decimal-delimiter
+###
+### as the original data-cells were like this:     '187.118
+### I decided to change the figures by hand, i.e. eliminating the ' and the .
+### ... otherwise the above mentioned approach should work fine.
+
 library(xlsx)
+my_cc<-c("character",rep("integer", 19))
+my_data <- read.xlsx("data/adac-unfallgeschehen.xlsx", "Page 13", startRow = 2, header = T, colIndex=c(1,2,3,4,5,6,7), colClasses = my_cc)
+
+my_data$Year<-as.Date(my_data$Year,origin=0)
+my_data$Year<-as.character(my_data$Year)
+#my_data_ts<-as.ts(my_data)
+
+library(forecast)
+mfit<-auto.arima(my_data$F.less18)
+fcast<-forecast(mfit, h=5)
+#fcast
+plot(fcast, main = "Number of Road Accidents in Germany", ylab = "Total # of Males", xlab = "Year", xaxt="n", plot.conf="FALSE")
+my_years<-as.integer(my_data$Year)
+axis(1,at=1:16,label=my_years,las=2)
+#axis(2,at=1:25,label=seq(1,500000,20000))
+grid()
---
+++ writing/accident-report.md
@@ -0,0 +1,30 @@
+Forecasting of road accident responsibles (gender/age) in Germany
+========================================================
+author: Bernhard Thoni
+date: 15.03.2015
+
+
+Introduction, Dataset and Model
+========================================================
+
+- The data are official numbers from Germany's ADAC 
+   (automobile club)
+- Beginning with 2003, the data are complete (yearwise) till 2013
+   (before this date, some years are missing)
+- Here you can find the shiny-app developed for the course project:
+   https://selfman2.shinyapps.io/accident-report/
+- I use the forecast library for this project
+- The applied methodology is: arima
+
+
+Model of the Forecast
+========================================================
+
+
+
+
+
+
+```
+Error in loadWorkbook(file) : Cannot find data/adac-unfallgeschehen.xlsx
+```
